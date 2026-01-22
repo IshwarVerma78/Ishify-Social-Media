46c9b297471635c605c417527f93f7f7cd762279
IshwarVerma78 <ishwarverma78a@gmail.com>
2026-01-22 23:56:20 +0530

    first Commit

---
+++ client/src/pages/Layout.jsx
@@ -0,0 +1,35 @@
+// import { Menu, SidebarOpen } from 'lucide-react'
+import { Menu, X } from 'lucide-react';
+import React, { useState } from 'react'
+import { Outlet } from 'react-router-dom'
+// import { X } from 'lucide-react';
+import { dummyUserData } from '../assets/assets';
+import Loading from '../components/Loading';
+import Sidebar from '../components/Sidebar';
+// import { Menu } from 'lucide-react';
+
+const Layout = () => {
+  const user = dummyUserData;
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  return user? (
+     <div className='w-full flex h-screen'>
+        <Sidebar sidebarOpen = {sidebarOpen} setSidebarOpen= {setSidebarOpen}/>
+        <div className='flex-1 bg-slate-50'>
+          <Outlet/>
+        </div>
+
+        { 
+        
+            sidebarOpen ? <X onClick={()=>setSidebarOpen(false)} className='absolute top-3 right-3 p-2 z-100 bg-white rounded-md shadow w-10 h-10 text-gray-600 sm:hidden'/> 
+            : <Menu onClick={()=>setSidebarOpen(true)} className='absolute top-3 right-3 p-2 z-100 bg-white rounded-md shadow w-10 h-10 text-gray-600 sm:hidden'/> 
+        
+        }
+
+    
+    </div>
+  ) : (
+    <Loading/>
+  )
+}
+
+export default Layout
