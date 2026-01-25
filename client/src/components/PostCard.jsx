014e85e135868d39a4434791159de8c2d62c4dcc
IshwarVerma78 <ishwarverma78a@gmail.com>
2026-01-25 23:58:22 +0530

    Connection page complete

--- client/src/components/PostCard.jsx
+++ client/src/components/PostCard.jsx
@@ -2,11 +2,13 @@ import React, { useState } from 'react'
 import {BadgeCheck, Heart, MessageCircle, Share2} from 'lucide-react';
 import moment from 'moment';
 import { dummyUserData } from '../assets/assets';
+import { useNavigate } from 'react-router-dom';
 
 const PostCard = ({post}) => {
 
     const [likes, setLikes] = useState(post.likes_count)
     const currentUser = dummyUserData;
+    const navigate = useNavigate();
 
 
     const postWithHashtags = post.content.replace(/(#\w+)/g, '<span class="text-indigo-600"> $1 </span>')
@@ -20,7 +22,7 @@ const PostCard = ({post}) => {
   return (
     <div className='bg-white rounded-xl shadow p-4 space-y-4 w-full max-w-2xl'>
         {/* User Info */}
-        <div className='inline-flex items-center gap-3 cursor-pointer'>
+        <div onClick={()=>navigate('/profile/'+post.user._id)} className='inline-flex items-center gap-3 cursor-pointer'>
             <img src={post.user.profile_picture} alt="" className='w-10 h-10 rounded-full shadow' />
             <div>
                 <div className='flex items-center space-x-1'>
